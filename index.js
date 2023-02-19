c6849f4e21716e7dc67a0bfadd1df51d5d2cd4b4
dmonicag <monicagandhi89@gmail.com>
2023-02-19 13:22:57 +0530

    port change

--- index.js
+++ index.js
@@ -97,7 +97,7 @@ app.post('/api/persons', (request, response) => {
 })
 
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 10000
 app.listen(PORT, () => {
     console.log(`Server running on PORT ${PORT}`)
 })
